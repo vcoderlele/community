8d5c79a4baaae88a458b1028928707a495d212a0
= <479007532@qq.com>
2019-11-06 22:04:23 +0800

    init

---
+++ src/main/java/com/xk366/community/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.xk366.community.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+
+    public String index(){
+        return 'hello';
+    }
+}
